570532d1e6c68ccdf2f123a457844ab579d9b3ec
Gabriel Fiorim Esteves <123314503+Fiorim1@users.noreply.github.com>
2025-03-27 20:26:31 -0300

    Add files via upload

---
+++ Capitulo 4/Arrays-E-Manipulacao-De-Dados.js
@@ -0,0 +1,16 @@
+/* Arrays: */
+/* Um array e uma lista que pode armazenar varios valores em uma unica variavel. Cada valor tem um indice começando do 0 (zero). */
+let fruits = [
+    "Banana",
+    "Apple",
+    "Orange"
+];
+console.log(fruits[2]);
+/* Saida No Console: 
+Orange
+*/
+/* Metodos De Arrays: */
+/* Sao funçoes prontas para manipular listas: */
+push(value); /* -> Adiciona um item no final da lista */
+pop(); /* -> Remove o item do final da lista */
+shift(); /* Remove o primeiro item da lista */
