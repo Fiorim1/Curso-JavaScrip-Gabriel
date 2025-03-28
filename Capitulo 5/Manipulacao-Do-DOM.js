e66180b28456b7acb4e8766ad31e5c6ff0764034
Gabriel Fiorim Esteves <123314503+Fiorim1@users.noreply.github.com>
2025-03-27 21:42:52 -0300

    Update Manipulacao-Do-DOM.js

--- Capitulo 5/Manipulacao-Do-DOM.js
+++ Capitulo 5/Manipulacao-Do-DOM.js
@@ -7,3 +7,23 @@
 -> document.querySelector("seletor"); Retorna o primeiro elemento que atende ao seletor CSS
 -> document.querySelectorAll("selector"); retorna uma NodeList com todos os elementos que correspondem ao Seletor.
 */
+
+/* Manipulaçao De Conteudo: */
+/* -> element.innerHTML = "new content"; Define ou obtem HTML dentro do elemento
+-> element.textContent = "new content"; Define ou obtem apenas o texto dentro do elemento
+-> element.value = "new value"; Define o valor de um input
+*/
+
+/* Manipulaçao De Estilos: */
+/* -> element.style.property = "value"; Modifica estilos diretamente
+Exemplo: element.style.color = "red";
+-> element.classList.remove("class"); Remove uma classe CSS
+-> element.classList.add("class"); Adiciona uma classe CSS
+-> element.classList.toggle("class"); Adiciona se nao tiver, remove se ja tiver
+*/
+
+/* Manipulaçao De Atributos: */
+/* -> element.setAttribute("attribute", "values"); Define ou modifica um atributo
+-> element.getAttribute("attribute"); Obtem o valor de um atributo
+-> element.removeAttribute("attribute"); Remove um atributo
+*/
