3439b9845c175cf4fcd53310b3b0e5f8ae199d81
Gabriel Fiorim Esteves <123314503+Fiorim1@users.noreply.github.com>
2025-03-27 21:27:35 -0300

    Add files via upload

---
+++ Capitulo 5/Manipulacao-Do-DOM.js
@@ -0,0 +1,9 @@
+/* Manipulaçao Do DOM: */
+/* A Manipulaçao Do DOM )Document Object Model) em JavaScript permite interagir e modificar a estrutura, o conteudo e o estilo dos elementos HTML de uma pagina. */
+/* Principais Metodos De Seleçao: 
+-> Para acessar elementos do DOM, usa-se: document.getElementById(); Seleciona um elemento pelo ID 
+-> document.getElementsByClassName("classe"); Retorna um HTML Collection com os elementos de uma classe
+-> document.getElementByTagName("tag"); Retorna um HTML Collection com os elementos da tag
+-> document.querySelector("seletor"); Retorna o primeiro elemento que atende ao seletor CSS
+-> document.querySelectorAll("selector"); retorna uma NodeList com todos os elementos que correspondem ao Seletor.
+*/
