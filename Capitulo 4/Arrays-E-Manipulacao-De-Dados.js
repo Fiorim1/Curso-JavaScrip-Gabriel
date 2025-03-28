dd6219fbb38165cf8f64e6024d3f6638bf889add
Gabriel Fiorim Esteves <123314503+Fiorim1@users.noreply.github.com>
2025-03-27 21:13:16 -0300

    Update Arrays-E-Manipulacao-De-Dados.js

--- Capitulo 4/Arrays-E-Manipulacao-De-Dados.js
+++ Capitulo 4/Arrays-E-Manipulacao-De-Dados.js
@@ -11,6 +11,23 @@ Orange
 */
 /* Metodos De Arrays: */
 /* Sao funçoes prontas para manipular listas: */
-push(value); /* -> Adiciona um item no final da lista */
-pop(); /* -> Remove o item do final da lista */
-shift(); /* Remove o primeiro item da lista */
+.push(value); /* -> Adiciona um item no final da lista */
+.pop(); /* -> Remove o item do final da lista */
+.shift(); /* Remove o primeiro item da lista */
+.unshift(value); /* Adiciona um item no inicio */
+.lenght; /* -> Retorna o tamanho da lista (array) */
+.forEach(fn); /* -> Executa uma funçao para cada item da lista */
+.map(fn); /* Cria um novo array transformado */
+.filter(fn); /* Retorna apenas os itens que passam no teste/filtro */
+/* Exemplo: */
+let numbers = [1, 2, 3, 4, 5];
+let double = numbers.map(n => n * 2);
+console.log(double)
+/* Explicacao:
+Criaçao Do Array:
+numbers = [1, 2, 3, 4, 5];
+Transformaçao com ".MAP()":
+-> Cada numero e multiplicado por 2, gerando "DOUBLE = [2, 4, 6, 8, 10]";
+Exibiçao Do Resultado: 
+console.log(double); Imprime [2, 4, 6, 8, 10] 
+*/
